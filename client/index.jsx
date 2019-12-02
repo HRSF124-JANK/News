8b469f510cd6a283945974fcaeec0eca5582495b
jonathanhkim <jhkim792@gmail.com>
2019-12-01 19:29:14 -0800

    creates initial app skeleton

---
+++ client/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor() {
+    super()
+  }
+
+  
+  render() {
+    return (
+      <div>asdf</div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))

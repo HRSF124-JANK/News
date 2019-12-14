d9d92015b5329e499cd55488e991563b125b7540
jonathanhkim <jhkim792@gmail.com>
2019-12-13 21:27:49 -0800

    hard code some stuff

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const env = process.env.NODE_ENV;
-const host = env === "production" ? "database" : "localhost";
-const mongoUri = `mongodb://${host}/news`;
+const mongoUri = `mongodb://database/news`;
 
 const db = mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
 
